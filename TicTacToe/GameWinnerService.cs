44e261d948aea04693e55b1d894fb853c2e7526e
davi7725 <davi7725@edu.eal.dk>
2016-09-29 17:05:14 +0200

    Add project files.

---
+++ TicTacToe/GameWinnerService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using TicTacToe.Services;
+
+namespace TicTacToe
+{
+    internal class GameWinnerService : IGameWinnerService
+    {
+        public char Validate(char[,] gameBoard)
+        {
+            char winnerCharacter = ' ';
+
+            //CHECKS ALL THE LINES IN THE CHAR ARRAY
+            bool lineIsWinner;
+            for (int r = 0; r <= 2; r++)
+            {
+                lineIsWinner = true;
+                for (int c = 1; c <= 2; c++)
+                {
+                    if (gameBoard[r, c] != gameBoard[r, c - 1])
+                    {
+                        lineIsWinner = false;
+                    }
+
+                }
+                if(lineIsWinner == true)
+                {
+                    winnerCharacter = gameBoard[r, 0];
+                }
+            }
+
+            //CHECKS ALL THE ROWS IN THE CHAR ARRAY
+            bool rowIsWinner;
+            for (int c = 0; c <= 2; c++)
+            {
+                rowIsWinner = true;
+                for (int r = 1; r <= 2; r++)
+                {
+                    if (gameBoard[r, c] != gameBoard[r-1, c])
+                    {
+                        rowIsWinner = false;
+                    }
+
+                }
+                if (rowIsWinner == true)
+                {
+                    winnerCharacter = gameBoard[c, 0];
+                }
+            }
+
+
+            //CHECK PRIMARY DIAGONALIS
+            bool primDiagonal = true; 
+
+            for(int d = 1; d<=2; d++)
+            {
+                if(gameBoard[d,d] != gameBoard[d-1,d-1])
+                {
+                    primDiagonal = false;
+                }
+            }
+
+
+            if (primDiagonal == true)
+            {
+                winnerCharacter = gameBoard[0, 0];
+            }
+
+            //CHECK SECONDARY DIAGONALIS
+            bool secDiagonalWinner;
+
+            for (int d = 1; d <= 2; d++)
+            {
+                secDiagonalWinner = true;
+                for (int d1 = 1; d1 >= 0; d1--)
+                {
+                    if(gameBoard[d,d1] != gameBoard[d-1,d1+1])
+                    {
+                        secDiagonalWinner = false;
+                    }
+
+                }
+                if(secDiagonalWinner == true)
+                {
+                    winnerCharacter = gameBoard[0, 2];
+                }
+            }
+
+
+            return winnerCharacter;
+        }
+    }
+}
---
+++ TicTacToe/GameWinnerServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Services;
+
+namespace TicTacToe
+{
+    [TestClass]
+    public class GameWinnerServiceTests
+    {
+        private GameWinnerService _gameWinnerService;
+        private char[,] _gameBoard;
+
+        [TestInitialize]
+        public void SetUpUnitTests()
+        {
+            _gameWinnerService = new GameWinnerService();
+            _gameBoard = new char[3, 3] { {' ',' ',' ' },
+                                             {' ',' ',' ' },
+                                             { ' ',' ',' '} };
+        }
+        [TestMethod]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            char[,] gameBoard = new char[3, 3] { {' ',' ',' ' },
+                                             {' ',' ',' ' },
+                                             { ' ',' ',' '} };
+
+            char actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PlayerWithAllSpacesInTopRowIsWinner()
+        {
+            IGameWinnerService gameWinnerService;
+            gameWinnerService = new GameWinnerService();
+            const char expected = 'X';
+            char[,] gameBoard = new char[3, 3] { {expected,expected,expected },
+                                             {' ',' ',' ' },
+                                             { ' ',' ',' '} };
+            char actual = gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            const char expected = 'X';
+            for(var columnIndex = 0; columnIndex <3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 0] = expected;
+            }
+            char actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod]
+        public void PlayerWithThreeInARowDiagonallyDownAndToRightIsWinner()
+        {
+            const char expected = 'X';
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex] = expected;
+            }
+            char actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+
+        [TestMethod]
+        public void PlayerWithThreeInARowDiagonallyUpAndToLeftIsWinner()
+        {
+            const char expected = 'X';
+            int cellIndex2 = 2;
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex2] = expected;
+                cellIndex2--;
+            }
+            char actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
